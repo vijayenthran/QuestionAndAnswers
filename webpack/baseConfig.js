6a2cefcaa7ae1b64f70776b0f19561a99b3c5e0e
Vijay <vijayjs2208@gmail.com>
2018-05-29 22:34:31 -0700

    Change config for deploy

--- webpack/baseConfig.js
+++ webpack/baseConfig.js
@@ -5,7 +5,7 @@ module.exports = {
         context: path.resolve(__dirname, '../src'), // Point to the src as the context as that has the entry point to start bundling - Needs to be a relative path
         entry: './index.js', // Since the context is mentioned above we just specify our entry file name here. Needs to be an absolute path
         output: {
-            path: path.resolve(__dirname, 'dist'), // Where the bundle should be.
+            path: path.resolve(__dirname, '../dist'), // Where the bundle should be.
             filename: 'bundle.js',
         },
         module: {
